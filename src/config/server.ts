9129c8f09bb20dc836bc364386f68de056b24d57
Felipe <felipe.martini2404@gmail.com>
2025-04-21 12:22:14 -0300

    refactor: reorganize server and app files

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,16 +1,7 @@
-import express from "express";
-import cors from "cors";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+import app from "../app";
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
